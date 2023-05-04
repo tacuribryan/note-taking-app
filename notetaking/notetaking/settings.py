ce8d5675a67f89396ec64bac8f2bef80050b78d9
Bryan Tacuri <tacuri_bryan@hotmail.com>
2023-05-04 15:14:33 -0400

    added Django CORS headers

--- notetaking/notetaking/settings.py
+++ notetaking/notetaking/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'notes',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'notetaking.urls'
